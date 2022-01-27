d2d7bdf638f31e70eafd03303021f17b60de3939
swayamppati <swayamppati@gmail.com>
2022-01-27 21:50:27 +0530

    App Created and Deployed to Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDy61_lfFw1cRs_ksll2SG3-vwpUnvepmA",
+    authDomain: "organic-shop-a3f27.firebaseapp.com",
+    projectId: "organic-shop-a3f27",
+    storageBucket: "organic-shop-a3f27.appspot.com",
+    messagingSenderId: "272940271788",
+    appId: "1:272940271788:web:13cef2a97737dafe527709",
+    measurementId: "G-5GBZ0EWC75"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDy61_lfFw1cRs_ksll2SG3-vwpUnvepmA",
+    authDomain: "organic-shop-a3f27.firebaseapp.com",
+    projectId: "organic-shop-a3f27",
+    storageBucket: "organic-shop-a3f27.appspot.com",
+    messagingSenderId: "272940271788",
+    appId: "1:272940271788:web:13cef2a97737dafe527709",
+    measurementId: "G-5GBZ0EWC75"
+  }
 };
 
 /*
